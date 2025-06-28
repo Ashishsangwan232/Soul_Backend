5898f35e0e99fdbec9fb7dbc45c031153a2ce74a
{ashish} <{ashishsangwan26790@gmail.com}>
2025-06-28 14:28:57 +0530

    added monitor for checking down time of website 0.1.1

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,27 +1,42 @@
 const User = require('../models/user');
 
-exports.saveToken = async (req, res) => {
+
+const saveToken = async (req, res) => {
+    // const userId = req.user.id; // Assuming Auth Middleware adds `req.user`
+    const { token, userId } = req.body;
+    // const { token } = req.body;
+
+    if (!token || !userId) {
+        return res.status(400).json({ message: 'Missing token or userId' });
+    }
+
+    //     const user = await User.findById(userId);
+
+    //     // if (!user) {
+    //     //     return res.status(404).json({ message: 'User not found' });
+    //     // }
+
+    //     user.fcmToken = token;
+    //     await user.save();
+
+    //     res.json({ message: 'FCM token saved successfully' });
+    // } catch (err) {
+    //     console.error('Error saving FCM token:', err);
+    //     res.status(500).json({ message: 'Server error' });
+    // }
     try {
-        const userId = req.user.id; // Assuming Auth Middleware adds `req.user`
-        // const { token, userId } = req.body;
-        const { token } = req.body;
-
-        if (!token || !userId) {
-            return res.status(400).json({ message: 'Missing token or userId' });
-        }
-
         const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: 'User not found' });
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        user.fcmToken = token;
+        user.fcmToken = token; // Store latest token
         await user.save();
 
         res.json({ message: 'FCM token saved successfully' });
     } catch (err) {
         console.error('Error saving FCM token:', err);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Internal server error' });
     }
+
 };
+
+module.exports = saveToken;
