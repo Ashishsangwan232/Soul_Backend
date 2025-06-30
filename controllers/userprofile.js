9414949637bcdf77c58370d4f344461a78d53eee
{ashish} <{ashishsangwan26790@gmail.com}>
2025-06-30 09:26:56 +0530

    New features 0.6

--- controllers/userprofile.js
+++ controllers/userprofile.js
@@ -1,20 +1,27 @@
 const User = require('../models/user');
 const Post = require('../models/posts');
+const Like =require('../models/Likes');
 
 exports.userprofile = async (req, res) => {
     try {
         const userId = req.params.userId;
 
-        // Get basic details
-        // console.log(userId);
-        const user = await User.findById(userId).select('username email profilePicKey');
+        const user = await User.findById(userId).select('username profilePicKey');
 
         if (!user) return res.status(404).json({ message: 'User not found' });
-        // console.log(userId);
-        // Get additional stats
+
         const posts = await Post.find({ authorId: userId });
         const postCount = posts.length;
-        // const totalLikes = posts.reduce((acc, post) => acc + post.likes.length, 0);
+
+        // Extract Post IDs
+        const postIds = posts.map(post => post._id);
+
+        // Count likes from Like model where targetId in user's posts
+        const totalLikes = await Like.countDocuments({
+            targetId: { $in: postIds },
+            targetType: 'Post'
+        });
+
         const userObj = user.toObject({ virtuals: true });
 
         res.json({
@@ -22,8 +29,9 @@ exports.userprofile = async (req, res) => {
             // email: userObj.email,
             profilePicture: userObj.profilePic,
             postCount,
-            // totalLikes,
+            totalLikes,
         });
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server Error' });
